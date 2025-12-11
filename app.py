4294feb0c437afa8285d5728eef7fbea2c7f9abf
phunthytl <phunthytl@gmail.com>
2025-12-11 16:51:19 +0700

    update

--- app.py
+++ app.py
@@ -551,4 +551,5 @@ def admin_dashboard():
 # MAIN
 # ===========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
